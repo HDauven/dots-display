94eb2ba42fc9af3b335f558777285245f254b8d9
Hein <heindauven@gmail.com>
2022-02-12 12:19:06 +0100

    Remove unused imports from main page.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,6 @@
 
 <script>
 	import { onMount } from 'svelte';
-	import Counter from '$lib/Counter.svelte';
 	import { getDotsMetadata, instantiateDotsCanvasContract, totalNumberOfDots } from '../lib/dots';
 
 	let totalNrOfDots = 0;
@@ -37,26 +36,12 @@
 
 <section>
 	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
 		Total number of dots: {totalNrOfDots}
 		{#if imageURL}
 			<img src={imageURL} alt={dotName} />
 			<p>{dotName}</p>
 		{/if}
-		to your new<br />SvelteKit app
 	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
 </section>
 
 <style>
@@ -70,20 +55,5 @@
 
 	h1 {
 		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
