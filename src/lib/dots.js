6c20dcde0ce6cdd416d3ea67bf0e7190d4e7d89b
Hein <heindauven@gmail.com>
2022-02-12 11:06:23 +0100

    Create Dots canvas instantiation contract.

--- src/lib/dots.js
+++ src/lib/dots.js
@@ -1,8 +1,14 @@
 import { Contract, ethers} from 'ethers'
 import DotsCanvasABI from './DotsCanvasABI.json'
 
-const DOTS_CONTRACT = ''
 const DOTS_CANVAS_CONTRACT = ''
+const NETWORK = 'https://l2.testnet.nahmii.io'
+
+export const instantiateDotsCanvasContract = () => {
+    const provider = readOnlyProvider(NETWORK)
+    const contract = new Contract(DOTS_CANVAS_CONTRACT, DotsCanvasABI, provider)
+    return contract
+}
 
 export const readOnlyProvider = (network) => {
     return new ethers.providers.JsonRpcProvider(network)
