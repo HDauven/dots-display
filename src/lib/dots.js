a0bf4d516f8b9ed3a7b830d938c72a7b7cf74571
Hein <heindauven@gmail.com>
2022-02-12 11:51:35 +0100

    Retrieve Dots metadata.

--- src/lib/dots.js
+++ src/lib/dots.js
@@ -4,6 +4,13 @@ import DotsCanvasABI from './DotsCanvasABI.json'
 const DOTS_CANVAS_CONTRACT = '0x797F6B319603c7A7fE5E6110517D435eC59e6Fc0'
 const NETWORK = 'https://l2.testnet.nahmii.io'
 
+export const getDotsMetadata = async (contract, tokenID) => {
+    const dotsURI = await getTokenURI(contract, tokenID)
+    const response = await fetch(dotsURI)
+    const dotsMetadata = await response.json()
+    return dotsMetadata
+}
+
 export const getTokenURI = async (contract, tokenID) => {
     return await contract.tokenURI(tokenID)
 }
