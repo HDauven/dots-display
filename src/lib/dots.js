e04eef3876f880e84f0320311dc9f1c3f76d2d6b
Hein <heindauven@gmail.com>
2022-02-12 11:20:37 +0100

    Add function to retrieve the total dots supply.

--- src/lib/dots.js
+++ src/lib/dots.js
@@ -1,8 +1,12 @@
 import { Contract, ethers} from 'ethers'
 import DotsCanvasABI from './DotsCanvasABI.json'
 
-const DOTS_CANVAS_CONTRACT = ''
+const DOTS_CANVAS_CONTRACT = '0x797F6B319603c7A7fE5E6110517D435eC59e6Fc0'
 const NETWORK = 'https://l2.testnet.nahmii.io'
+
+export const totalNumberOfDots = async (contract) => {
+    return await contract.totalSupply()
+}
 
 export const instantiateDotsCanvasContract = () => {
     const provider = readOnlyProvider(NETWORK)
